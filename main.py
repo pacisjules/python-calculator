ba90f1d7d7e6464ee55867e95ccac26c2b1ff0d3
Ishimwe Jules Pacis <pacisjules@gmail.com>
2021-09-02 12:47:38 +0200

    Update main
    Update main  checking and Correct

--- main.py
+++ main.py
@@ -1,40 +1,44 @@
+import sys
+import os
 from services import our_services as srvs
 from components.header import heading
 
 
-#Header
+# Header
 heading()
 
 
-
-#Service Menu
+# Service Menu
 def menu():
-    a=0
+    a = 0
     for i in srvs:
-        a+=1
+        a += 1
         print(str(a)+". >> "+str(i))
+
 
 menu()
 
 print('\n******** --------------- ********\n')
 
-#ask and input choosen number
+# ask and input choosen number
 chooser = input("Enter choosen number: ")
 
-#Get menu numbers you have
-numbers=[]
-a=0
+# Get menu numbers you have
+numbers = []
+a = 0
 for i in srvs:
-        a+=1
-        numbers.append(a)
+    a += 1
+    numbers.append(a)
 
-#check if is integer
+# check if is integer
 try:
     val=int(chooser)
-    if val>0 and val<numbers:
-        print("Correct")
-
-        #Actions Here
+    exists = int(chooser) in numbers
+    if exists:
+        print("Yes is in menu")
+    else:
+        print("No is not in menu")
 
 except ValueError:
     print("Please use number to choose")
+
